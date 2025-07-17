d1c35f3acd74888c77f163358f0df81e2dbe2768
Dmytro_Kravchuk <dmitriy.n.kravchuk@gmail.com>
2025-07-17 21:49:31 +0300

    code refactoring

--- src/main/java/org/comforent/controllers/OAuth2ErrorController.java
+++ src/main/java/org/comforent/controllers/OAuth2ErrorController.java
@@ -13,40 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Locale;
 import java.util.Map;
 
-//@RestController
-//@RequestMapping("/oauth2")
-//public class OAuth2ErrorController {
-//    private static final Logger logger = LoggerFactory.getLogger(OAuth2ErrorController.class);
-//
-//    private final MessageSource messageSource;
-//
-//    public OAuth2ErrorController(MessageSource messageSource) {
-//        this.messageSource = messageSource;
-//    }
-//
-//    @GetMapping("/error")
-//    public ResponseEntity<Map<String, String>> handleError(@RequestParam(required = false) String code,
-//                                                           Locale locale) {// Используем код вместо "message" и получаем из messages.properties
-//        String errorMsg;
-//
-//        if (code != null && !code.isEmpty()) {
-//            try {
-//                errorMsg = messageSource.getMessage(code, null, locale);
-//            } catch (Exception e) {
-//                errorMsg = messageSource.getMessage("oauth2.error.default", null, "OAuth2 authorization failed", locale);
-//            }
-//        } else {
-//            errorMsg = messageSource.getMessage("oauth2.error.default", null, "OAuth2 authorization failed", locale);
-//        }
-//
-//        logger.warn("OAuth2 authentication error occurred");
-//
-//        return ResponseEntity
-//            .status(HttpStatus.UNAUTHORIZED)
-//            .body(Map.of("error", errorMsg));
-//    }
-//}
-
 @RestController
 @RequestMapping("/oauth2")
 public class OAuth2ErrorController {
